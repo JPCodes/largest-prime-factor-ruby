197991c7777cd1de6b27ee51c2a22fd5cf082fdb
Jim Padilla <jp_code2013@yahoo.com>
2016-11-22 18:17:15 -0800

    remove hard-coded primes array, create method to populate an array of prime numbers

--- problem_3.rb
+++ problem_3.rb
@@ -1,15 +1,34 @@
-define_method(:find_primes) do
+define_method(:find_primes) do |upper_limit|
   original_num = self
-  primes_array = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 6857]
+
+  # Define an array of primes
+  primes_array = upper_limit.create_primes_array()
+
+  # Stores prime factors that divide into our original number
   prime_factors = []
 
+  # Determines which prime factors to store into the prime factors array
   primes_array.each() do |prime|
     if original_num % prime === 0
       prime_factors.push(prime)
     end
   end
-  prime_factors
+  prime_factors[-1]
 end
 
+define_method(:create_primes_array) do
+  # Create primes_array here for storage
+  primes_array = []
 
-puts 600851475143.find_primes()
+  # Determine prime numbers here
+    # Sieve of Eratothenes, ancient algorithm used to find prime numbers
+  (1..self).each do |num|
+    if num % 2 != 0 or num % 3 != 0 or num % 5 != 0
+      primes_array.push(num)
+    end
+  end
+  primes_array
+end
+
+# Answer: 6857 is the largest prime factor of 600,851,475,143
+puts 600851475143.find_primes(10000)
